e2cc61edefeab65f0e67977db60d72e1ff85b5ed
sunny2601 <sunnysarrot@gmail.com>
2015-06-11 09:54:29 +0530

    Delete 20150603090916_add_partial_payment_fields_to_spree_payment_method.rb

--- db/migrate/20150603090916_add_partial_payment_fields_to_spree_payment_method.rb
+++
@@ -1,5 +0,0 @@
-class AddPartialPaymentFieldsToSpreePaymentMethod < ActiveRecord::Migration
-  def change
-  	add_column :spree_payment_methods, :partial_percentage, :integer, :default => 0
-  end
-end
